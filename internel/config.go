a9736abb2b7e8f5ba5a9030cbc2f161dda65aa34
yeyongping <burnedfrog@163.com>
2019-09-12 18:19:33 +0800

    change uuid general mode

--- internel/config.go
+++ internel/config.go
@@ -27,7 +27,7 @@ func init() {
 
 func getConfig() Config {
 	viper.SetConfigName("config")
-	viper.AddConfigPath("../")
+	viper.AddConfigPath(".")
 	if err := viper.ReadInConfig(); err != nil {
 		panic(err)
 	}
