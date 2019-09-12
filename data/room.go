b0bc6440a126519b347dbecda8187dfb38c074b3
yeyongping <burnedfrog@163.com>
2019-09-12 09:36:53 +0800

    add room_name

--- data/room.go
+++ data/room.go
@@ -4,6 +4,7 @@ import "github.com/jinzhu/gorm"
 
 type Room struct {
 	gorm.Model
+	RoomName string         `gorm:"type:varchar(50);not null" json:"room_name"`
 	Owner string            `gorm:"type:varchar(50);not null" json:"owner"`
 	Managers []User         `gorm:"many2many:room_managers" json:"managers"`
 	Users []User            `gorm:"many2many:room_users" json:"users"`
---
+++ dbOps/room.go
@@ -0,0 +1,5 @@
+package dbOps
+
+func CreateRoom() {
+
+}
